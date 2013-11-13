4e14871c5220d8132d018ff8e9e469441944be89
Tim Caswell <tim@creationix.com>
2013-11-13 12:55:32 -0600

    Get repo working better

--- src/repo.js
+++ src/repo.js
@@ -1,16 +1,13 @@
+var xhr = require('./xhr.js');
 
 module.exports = function (root, accessToken) {
+  var request = xhr(root, accessToken);
   var repo = {
-    apiGet: apiGet,
-    apiPost: apiPost
+    apiGet: request.bind(null, "GET"),
+    apiPost: request.bind(null, "POST"),
+    apiPatch: request.bind(null, "PATCH"),
+    apiDelete: request.bind(null, "DELETE"),
   };
-
-  console.log(accessToken);
-
-  // if (!accessToken) {
-  //   accessToken = prompt("Enter github token");
-  //   if (!accessToken) return;
-  // }
 
   require('./objects.js')(repo);
 
@@ -19,59 +16,5 @@ module.exports = function (root, accessToken) {
   require('js-git/mixins/walkers.js')(repo);
 
   return repo;
-
-  function apiGet(url, callback) {
-    url = 'https://api.github.com' + url.replace(":root", root);
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.setRequestHeader("Authorization", "token " + accessToken);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState !== 4) return;
-      if (xhr.status !== 200) {
-        return callback(new Error("Invalid HTTP response: " + xhr.status));
-      }
-      var response;
-      try {
-        response = JSON.parse(xhr.responseText);
-      }
-      catch (err) {
-        return callback(err);
-      }
-      return callback(null, response);
-    };
-    xhr.send();
-  }
-
-  function apiPost(url, body, callback) {
-    url = 'https://api.github.com' + url.replace(":root", root);
-    var json;
-    try {
-      json = JSON.stringify(body);
-    }
-    catch (err) {
-      return callback(err);
-    }
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader("Authorization", "token " + accessToken);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState !== 4) return;
-      if (xhr.status < 200 || xhr.status >= 300) {
-        return callback(new Error("Invalid HTTP response: " + xhr.status));
-      }
-      var response;
-      try {
-        response = JSON.parse(xhr.responseText);
-      }
-      catch (err) {
-        return callback(err);
-      }
-      return callback(null, response);
-    };
-    xhr.send(json);
-  }
 
 };
