5e6c76d63be206e95d304cb7b59ec01478a3e493
Tim Caswell <tim@creationix.com>
2014-01-10 09:25:11 -0600

    Implement conditional requests

--- src/request.js
+++ src/request.js
@@ -1,5 +1,8 @@
 var https = require('https');
 module.exports = function (root, accessToken) {
+  var cache = {};
+
+
   return function request(method, url, body, callback) {
     if (typeof body === "function" && callback === undefined) {
       callback = body;
@@ -20,6 +23,12 @@ module.exports = function (root, accessToken) {
       try { json = JSON.stringify(body); }
       catch (err) { return callback(err); }
     }
+    if (method === "GET") {
+      var cached = cache[url];
+      if (cached) {
+        headers["If-None-Match"] = cached.etag;
+      }
+    }
     var options = {
       hostname: "api.github.com",
       path: url,
@@ -34,9 +43,19 @@ module.exports = function (root, accessToken) {
       });
       res.on("end", function () {
         body = Buffer.concat(body).toString();
-        // console.log(method, url, res.statusCode);
+        console.log(method, url, res.statusCode);
+        console.log("Rate limit %s/%s left", res.headers['x-ratelimit-remaining'], res.headers['x-ratelimit-limit']);
         if (res.statusCode >= 400 && res.statusCode < 500) return callback();
-        if (res.statusCode < 200 || res.statusCode >= 300) {
+        else if (res.statusCode === 200 && method === "GET") {
+          cache[url] = {
+            body: body,
+            etag: res.headers.etag
+          };
+        }
+        else if (res.statusCode === 304) {
+          body = cache[url].body;
+        }
+        else if (res.statusCode < 200 || res.statusCode >= 300) {
           return callback(new Error("Invalid HTTP response: " + res.statusCode));
         }
         if (body) {
@@ -47,5 +66,6 @@ module.exports = function (root, accessToken) {
       });
     });
     req.end(json);
+    req.on("error", callback);
   };
 };
